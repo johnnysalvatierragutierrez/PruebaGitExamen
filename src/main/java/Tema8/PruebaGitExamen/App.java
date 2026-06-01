525d6df088ac7071a3c98ba8f8176a65b949013f
johnnysalvatierragutierrez <simplementebecoinf@gmail.com>
2026-06-01 20:12:00 +0200

    First comit

---
+++ src/main/java/Tema8/PruebaGitExamen/App.java
@@ -0,0 +1,10 @@
+package Tema8.PruebaGitExamen;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Tema8/PruebaGitExamen/AppTest.java
@@ -0,0 +1,19 @@
+package Tema8.PruebaGitExamen;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
